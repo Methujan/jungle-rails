8049ed46598b28612d2f4e1e04feb0055bb0062d
Methujan <methujan_02@hotmail.com>
2021-07-27 01:42:02 +0000

    Test for authenticate with credentials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,43 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     it 'Ensure that an user is created with all validations fields' do
-      @user = User.new(first_name: 'Ash', last_name:'Ketchum', email: 'ash@hotmail.com', password: 'catch')
+      @user = User.new(first_name: 'Ash', last_name:'Ketchum', email: 'ash@hotmail.com', password: 'catchemall')
       @user.valid?
       expect(@user.errors.messages).not_to include("can\'t be blank")
+    end
+    it 'Ensure that an user is not created when first name is missing' do
+      @user = User.new( last_name:'Ketchum', email: 'ash@hotmail.com', password: 'catchemall')
+      @user.valid?
+      expect(@user.errors.messages[:first_name]).to include("can\'t be blank")
+    end
+    it 'Ensure that an user is not created when last name is missing' do
+      @user = User.new( first_name:'Ash', email: 'ash@hotmail.com', password: 'catchemall')
+      @user.valid?
+      expect(@user.errors.messages[:last_name]).to include("can\'t be blank")
+    end
+    it 'Ensure that an user is not created when email is missing' do
+      @user = User.new( first_name:'Ash', last_name:'Ketchum', password: 'catchemall')
+      @user.valid?
+      expect(@user.errors.messages[:email]).to include("can\'t be blank")
+    end
+    it 'Ensure that an user is not created when password is missing' do
+      @user = User.new( first_name:'Ash', last_name:'Ketchum', email: 'ash@hotmail.com')
+      @user.valid?
+      expect(@user.errors.messages[:password]).to include("can\'t be blank")
+    end
+    it 'Ensure that an user is not created when password is too short' do
+      @user = User.new( first_name:'Ash', last_name:'Ketchum', email: 'ash@hotmail.com', password: 'catch')
+      @user.valid?
+      expect(@user.errors.messages[:password]).to include("is too short (minimum is 6 characters)")
     end
   end
 
   describe '.authenticate_with_credentials' do
-    # examples for this class method here
+    it 'Ensure that an user is saved with email and password' do
+    @user = User.new(first_name:'Ash', last_name:'Ketchum', email: 'ash@hotmail.com', password: 'catchemall')
+    @user.save
+    expect(User.authenticate_with_credentials("ash@hotmail.com", "catchemall")).to be_present
   end
+end
 
 end
